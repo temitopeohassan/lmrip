b249dfad43a141bf3d1035d0739f89ee14d5d1fa
temitopeohassan <temitope.hassan.2@gmail.com>
2024-03-04 18:11:01 +0100

    login button

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
         placeholder="password"
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={handleClick} style={{ padding: 10, width:100 }}>
+ <button onClick={handleClick} style={{ padding: 10, width:100, backgroundColor: 'black', color: 'white' }}>
         Login
       </button>
     </div>
