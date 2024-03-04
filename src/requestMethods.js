faec23f8a5a81effd12862cc7494b59e5080dbf2
temitopeohassan <temitope.hassan.2@gmail.com>
2024-03-04 18:41:35 +0100

    req methods

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -11,7 +11,7 @@
 
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "http://https://lmripdb-3dir.onrender.com/api/";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
